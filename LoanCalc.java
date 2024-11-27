66ac0d82bab86caf10a6080d35016e74bca53df3
David Serfaty <davidserfaty11@gmail.com>
2024-11-27 10:06:54 +0200

    change calc made

--- LoanCalc.java
+++ LoanCalc.java
@@ -9,12 +9,12 @@ public class LoanCalc {
     // interest rate (double, as a percentage), and number of payments (int).  
 	public static void main(String[] args) {		
 		// Gets the loan data
-		//double loan = Double.parseDouble(args[0]);
-		//double rate = Double.parseDouble(args[1]);
-		//int n = Integer.parseInt(args[2]);
-		double loan = 50000;
-		double rate = 0.05;
-		int n = 36;
+		double loan = Double.parseDouble(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
+		//double loan = 50000;
+		//double rate = 0.05;
+		//int n = 36;
 
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 				
